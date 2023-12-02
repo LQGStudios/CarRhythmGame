451979aec5551e6703f3417f324e2071df198259
MaxPower <89873437+MaxPowah@users.noreply.github.com>
2023-12-02 15:46:10 +0100

    kan nu skilja på raderna i beatmap csv-filerna

--- sound.hpp
+++ sound.hpp
@@ -24,16 +24,47 @@ struct Beatmap
         float beatPosition; //relativ till songPosition
         void LoadBeatMap(const char*bPath) //läser in en csv-fil med beatmappen i. Denna beatmap kommer från ett excel-dokument med timing och annan notinformation
         {
-            bPath = "assets/beatmaps/Bm140.cs";
-
             std::ifstream fullBeatmap;
             fullBeatmap.open(bPath);
 
             if (fullBeatmap.fail())
             {
-                std::cerr << "Kan inte öppna filen" << bPath <<std::endl;
+                std::cerr << "Kan inte öppna filen" << bPath <<std::endl; //skickar ett smidigt errormeddelande
             }
             
+            int i; //för denna while-loop
+            while (fullBeatmap.peek() != EOF) //medans vi läser filen, innan den har tagit slut alltså, kollar vi på tabellen rad för rad
+            {
+                std::string lmb; //något av de 3 värdena i csv-filen(l står för värdet i kolumn 0, vilken fil på vägen (0-4) m i kolumn 1, vilken takt, b i kolumn 2, alltså vilket taktslag noten ska dyka upp på)
+                getline(fullBeatmap, lmb, ',');
+                std::cout << lmb <<std::endl;
+                
+                switch (i)
+                {
+                case 0: //l
+                    //talet är i första kolumnen, ska då behandlas som taktslag
+                    //spawna not i lane nr lmb
+                    
+                    //Note(lmb);
+                    std::cout << "reading lane";
+
+                    break;
+                case 1:
+                    std::cout << "reading measure";
+                    break;
+                case 2: //m 
+                    std::cout << "reading beat";
+
+                    i= -1;//så att i ska bli 0 igen
+                    break;
+                default:
+                    i=-1; //så att i ska bli 0 igen
+                    break;
+                }
+                
+                i++;
+            }
+            fullBeatmap.close();
 
         }
 };
